4c78d77046ef26356c4fce7762f204dc38b9e2d4
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-10-20 00:13:03 +0600

    added post method to store the product data in mongodb

--- index.js
+++ index.js
@@ -23,14 +23,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const productCollection = client.db("ProductDb").collection("Product");
+
     // Send a ping to confirm a successful connection
+    app.post("/product", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+
+      console.log(newProduct);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
