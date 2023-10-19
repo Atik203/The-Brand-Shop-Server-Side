c39bca8f5e745cc1392b00d47eeec6f7b09d9313
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-10-20 01:46:31 +0600

    created get based on each id for render the specific product details

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -37,6 +37,14 @@ async function run() {
       const brandName = req.params.brand;
       const cursor = productCollection.find({ brand: brandName });
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
       res.send(result);
     });
 
