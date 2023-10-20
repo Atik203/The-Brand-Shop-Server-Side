f18775c1dad533c9f8c4ceccaad42deadea5d09d
Atikur Rahaman <atikur@rahaman.noreply.github.com>
2023-10-20 13:08:03 +0600

    added delete method for users delete their selected cart product

--- index.js
+++ index.js
@@ -62,6 +62,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
